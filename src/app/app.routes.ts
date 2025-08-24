91d7f0ef3e88aaa61fce8284757e1db6b782ebe2
JayprakashL <jaylakheda96@email.com>
2025-08-24 20:26:33 +0530

    Wild card Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { EmployeeComponent } from './employee/employee.component';
 import { RoomComponent } from './room/room.component';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   { path: 'employee', component: EmployeeComponent },
   { path: 'rooms', component: RoomComponent },
   { path: '', redirectTo: '/rooms', pathMatch: 'full' },
-  //   { }
+  { path: '**', component: PageNotFoundComponent }, // Wild card routing
 ];
